a69989375772c2eaf10b463827285167361a6fca
Dadu400 <daduka.xuxunashvili@gmail.com>
2024-11-27 14:17:57 +0400

    Finished header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,22 @@
+import logo from '../../assets/icons/broken-heart.png';
+import Navbar from "./Navbar";
+import PostButton from "./PostButton";
+
+function Header() {
+    return (
+        <header className="w-full px-8 py-6">
+            <div className="w-[90%] md:w-[85%] lg:mx-auto flex items-center justify-between">
+                <div className="flex items-center font-bold text-lg">
+                    <a href={'/'} className="flex items-center gap-x-2">
+                        <img src={logo} alt="logo" className="w-8 h-8" />
+                        <span className="font-bpg tracking-widest font-regular">რაც ვერ გითხარი</span>
+                    </a>
+                </div>
+                <Navbar />
+                <PostButton />
+            </div>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/header/PostButton.jsx
@@ -0,0 +1,7 @@
+function PostButton() {
+    return (
+        <button className="hidden lg:flex font-bpg px-6 py-2 bg-main text-white rounded-xl">დაპოსტე</button>
+    );
+}
+
+export default PostButton;
