ed708301ad06951006ccba97b5a278a1d41706cc
Dadu400 <daduka.xuxunashvili@gmail.com>
2024-12-20 15:20:46 +0400

    fix routing problem

--- src/components/header/PostButton.tsx
+++ src/components/header/PostButton.tsx
@@ -2,10 +2,12 @@ import {Link} from "react-router-dom";
 
 function PostButton({ className }: { className?: string }) {
     return (
+        <Link to="/submit">
         <button
             className={`${className} font-dejavu px-6 py-2 bg-[#D93835] text-white rounded-xl tracking-widest`}>
-            <Link to="/submit">დაპოსტე</Link>
+           დაპოსტე
         </button>
+        </Link>
     );
 }
 
