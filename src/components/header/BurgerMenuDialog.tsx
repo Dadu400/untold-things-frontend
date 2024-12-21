2183a87ef05d51ace62a5ca0c8c859359e717fac
Dadu400 <daduka.xuxunashvili@gmail.com>
2024-12-21 16:43:31 +0400

    Minor adjustments in responsive design

--- src/components/header/BurgerMenuDialog.tsx
+++ src/components/header/BurgerMenuDialog.tsx
@@ -1,37 +1,48 @@
 import CloseIcon from '@mui/icons-material/Close';
 import PostButton from "./PostButton";
+import {NavLink} from "react-router-dom";
 
 export interface BurgerMenuDialogProps {
     setMenuOpen: (isOpen: boolean) => void;
 }
 function BurgerMenuDialog({ setMenuOpen }: BurgerMenuDialogProps) {
     return (
-        <div className="navbar fixed top-0 right-0 h-screen w-64 p-6 bg-bgColor shadow-lg cursor-normal z-10">
+        <div className="navbar fixed top-0 right-0 h-screen w-48 p-6 bg-bgColor shadow-lg cursor-normal z-10">
             <div className="flex justify-end mb-5">
                 <CloseIcon onClick={() => setMenuOpen(false)} />
             </div>
-            <div className="flex flex-col justify-start mt-5">
-                <a href="/" className="uppercase font-medium text-base font-bpg tracking-wider p-2 flex gap-5">
-                    <span>პოსტები</span>
-                </a>
-                <div className="flex flex-col">
-                    <a href="/" className="uppercase font-medium text-base font-bpg tracking-wider p-2 flex gap-5">
-                        <span>ანონიმური SMS</span>
-                    </a>
-                </div>
-                <div className="flex flex-col">
-                    <a href="/about" className="uppercase font-medium text-base font-bpg tracking-wider p-2 flex gap-5">
-                        <span>ჩვენზე</span>
-                    </a>
-                </div>
-                <div className="flex justify-start gap-[15px] mt-5">
-                    <div className="flex gap-[10px] justify-center items-center">
-                        <PostButton className="flex lg:hidden" />
-                    </div>
+            <div className="flex flex-col justify-start mt-14">
+                <ul className={"space-y-4"}>
+                    <li>
+                        <NavLink
+                            to="/"
+                            className= "text-lg font-dejavu tracking-wider">
+                            პოსტები
+                        </NavLink>
+                    </li>
+                    <li className="text-lg font-dejavu tracking-wider">
+                        ანონიმური SMS
+                    </li>
+                    <li className="text-lg font-dejavu tracking-wider">
+                        ჩვენზე
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/terms"
+                            className= "text-lg font-dejavu tracking-wider">
+                            წესები
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+            <div className="flex justify-start gap-[15px] mt-5">
+                <div className="flex gap-[10px] justify-center items-center">
+                    <PostButton className="flex lg:hidden"/>
                 </div>
             </div>
         </div>
-    );
+)
+    ;
 }
 
 export default BurgerMenuDialog;
--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,15 +1,15 @@
 import { NavLink } from "react-router-dom";
 
 function Navbar() {
-    const ClassName = "uppercase text-lg font-dejavu tracking-wider transition-colors duration-200 px-4 py-2";
+    const ClassName = "text-lg font-dejavu tracking-wider transition-colors duration-200 px-4 py-2";
 
     return (
         <nav>
-            <ul className="hidden lg:flex items-center gap-10">
+            <ul className="hidden lg:flex items-center gap-6">
                 <li>
                     <NavLink
                         to="/"
-                        className={({ isActive }) =>
+                        className={({isActive}) =>
                             `${ClassName} ${isActive ? "text-white bg-black rounded-xl" : "text-gray-700"}`
                         }
                     >
@@ -19,14 +19,17 @@ function Navbar() {
                 <li className={`${ClassName} cursor-pointer`}>
                     ანონიმური SMS
                 </li>
+                <li className={`${ClassName} cursor-pointer`}>
+                    ჩვენზე
+                </li>
                 <li>
                     <NavLink
-                        to="/aboutus"
-                        className={({ isActive }) =>
+                        to="/terms"
+                        className={({isActive}) =>
                             `${ClassName} ${isActive ? "text-white bg-black rounded-xl" : "text-gray-700"}`
                         }
                     >
-                        ჩვენზე
+                        წესები
                     </NavLink>
                 </li>
             </ul>
