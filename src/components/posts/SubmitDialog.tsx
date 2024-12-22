0b126c3cbc09238d2bad6bdb3dc907888927b6f4
Dadu400 <daduka.xuxunashvili@gmail.com>
2024-12-22 22:18:12 +0400

    New animation after submit

--- src/components/posts/SubmitDialog.tsx
+++ src/components/posts/SubmitDialog.tsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Dialog from "../posts/Dialog";
 import SinglePost from "./SinglePost";
+import "./SubmitDialog.css";
 
 interface SubmitDialogProps {
     isModalOpen: boolean;
     setIsModalOpen: (isOpen: boolean) => void;
     recipient: string;
     message: string;
+    onSubmit?: () => void;
 }
 
-const SubmitDialog: React.FC<SubmitDialogProps> = ({isModalOpen, setIsModalOpen, recipient, message,}) => {
+const SubmitDialog: React.FC<SubmitDialogProps> = ({ isModalOpen, setIsModalOpen, recipient, message, onSubmit}) => {
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleButtonClick = async () => {
+        if (!isClicked) {
+            setIsClicked(true);
+            await new Promise((resolve) => setTimeout(resolve, 2000));
+
+            setIsClicked(false);
+            setIsModalOpen(false);
+            onSubmit?.();
+        }
+    };
+
     return (
         <Dialog open={isModalOpen} onClose={() => setIsModalOpen(false)} className="max-w-[460px]">
             <div className="p-4 flex flex-col items-center">
@@ -29,15 +44,36 @@ const SubmitDialog: React.FC<SubmitDialogProps> = ({isModalOpen, setIsModalOpen,
                 </div>
                 <div className="flex flex-col justify-end">
                     <div className="flex">
-                    <input type={"checkbox"} className="mr-2" />
-                    <label className="text-md font-dejavu tracking-wider">
-                        გავეცანი და ვეთანხმები <a href="/terms" className="text-blue-500">წესებს</a>
-                    </label>
+                        <input type="checkbox" className="mr-2" />
+                        <label className="text-md font-dejavu tracking-wider">
+                            გავეცანი და ვეთანხმები <a href="/terms" className="text-blue-500">წესებს</a>
+                        </label>
                     </div>
                     <button
-                        className="font-dejavu px-6 py-2 bg-[#D93835] text-white rounded-xl tracking-widest mt-4"
-                        onClick={() => setIsModalOpen(false)} >
-                        დადასტურება
+                        className={`button font-dejavu bg-[#D93835] text-white py-2 rounded-xl tracking-widest mt-4 flex justify-center items-center gap-1 ${
+                            isClicked ? "clicked" : "" }`}
+                        onClick={handleButtonClick} >
+                        {isClicked ? "მიმდინარეობს გადამოწმება!" : "დადასტურება"}
+                        <svg
+                            version="1.1"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlnsXlink="http://www.w3.org/1999/xlink"
+                            width="24px"
+                            height="20px"
+                            x="0px"
+                            y="0px"
+                            viewBox="0 0 512 512"
+                            enableBackground="new 0 0 512 512"
+                            xmlSpace="preserve"
+                            fill="white"
+                            className="w-6 h-6"
+                        >
+                            <path
+                                id="paper-plane-icon"
+                                fill="white"
+                                d="M462,54.955L355.371,437.187l-135.92-128.842L353.388,167l-179.53,124.074L50,260.973L462,54.955z M202.992,332.528v124.517l58.738-67.927L202.992,332.528z"
+                            />
+                        </svg>
                     </button>
                 </div>
             </div>
