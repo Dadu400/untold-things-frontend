118bd5cf0eac2c16b1a6c6cf62f38335c602a7eb
Dadu400 <daduka.xuxunashvili@gmail.com>
2025-12-23 14:05:45 +0400

    fix header on scroll

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -13,16 +13,16 @@ function Header({ resetHomeKey }: { resetHomeKey?: () => void }) {
     };
 
     return (
-        <header className="w-full px-8 py-2 z-10 flex items-center justify-between bg-bgColor dark:bg-bgDark">
+        <header className="w-full px-4 lg:px-8 py-2 z-10 flex items-center justify-between bg-bgColor dark:bg-bgDark sticky top-0 lg:relative">
             <div className="w-full lg:w-[90%] lg:mx-auto flex items-center justify-between">
                 <NavLink
                     to="/"
                     onClick={handleLogoClick}
                 >
-                    <img src={logo} alt="logo" className="w-[70px] h-[70px]" />
+                    <img src={logo} alt="logo" className="w-12 h-12 lg:w-14 lg:h-14" />
                 </NavLink>
                 <Navbar resetHomeKey={resetHomeKey} />
-                <div className='flex items-center gap-4'>
+                <div className='flex items-center lg:gap-4'>
                     <ThemeSwitcher className="hidden lg:flex" />
                     <PostButton className="hidden lg:flex" />
                 </div>
